c8cea7da57690516fc6961d5044a099c27bbf85f
SaiAshish9 <saiashish@Sais-MacBook-Pro.local>
2022-11-10 17:26:16 +0530

    updated maven framework support

--- random-data-service/src/main/java/com/example/randomdataservice/models/UserRating.java
+++ random-data-service/src/main/java/com/example/randomdataservice/models/UserRating.java
@@ -1,2 +1,16 @@
-package com.example.randomdataservice.models;public class UserRating {
+package com.example.randomdataservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRating {
+
+        private List<Rating> userRating;
+
 }
--- random-data-service/src/main/java/com/example/randomdataservice/resources/RatingsResource.java
+++ random-data-service/src/main/java/com/example/randomdataservice/resources/RatingsResource.java
@@ -1,9 +1,13 @@
 package com.example.randomdataservice.resources;
 
 import com.example.randomdataservice.models.Rating;
+import com.example.randomdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,6 +16,17 @@ public class RatingsResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId){
         return new Rating(movieId,1);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1",1),
+                new Rating("2",2)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 
 }
